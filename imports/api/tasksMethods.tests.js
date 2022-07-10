a6be6bf10d1fa3d13231a411648e801e1b1693d3
Brendan Safr <brendansafr@gmail.com>
2022-07-10 18:55:04 -0400

    12.3: Prepare Database

--- imports/api/tasksMethods.tests.js
+++ imports/api/tasksMethods.tests.js
@@ -1,9 +1,21 @@
 import { Meteor } from "meteor/meteor";
+import { Random } from "meteor/random";
+import { TasksCollection } from "/imports/db/TasksCollection";
 
 if (Meteor.isServer) {
   describe("Tasks", () => {
     describe("methods", () => {
-      it("can delete owned task", () => {});
+      const userId = Random.id();
+      let taskId;
+
+      beforeEach(() => {
+        TasksCollection.remove({});
+        taskId = TasksCollection.insert({
+          text: "Test Task",
+          createdAt: new Date(),
+          userId,
+        });
+      });
     });
   });
 }
