bbb645505eda7d04dfcf2a97b92e331949f32551
Brendan Safr <brendansafr@gmail.com>
2022-07-10 18:01:44 -0400

    7.6: Server startup

--- server/main.js
+++ server/main.js
@@ -1,11 +1,16 @@
-import { Meteor } from 'meteor/meteor';
-import { Accounts } from 'meteor/accounts-base';
-import { TasksCollection } from '/imports/api/TasksCollection';
+import { Meteor } from "meteor/meteor";
+import { Accounts } from "meteor/accounts-base";
+import { TasksCollection } from "/imports/api/TasksCollection";
 
-const SEED_USERNAME = 'meteorite';
-const SEED_PASSWORD = 'password';
+const insertTask = (taskText, user) =>
+  TasksCollection.insert({
+    text: taskText,
+    userId: user._id,
+    createdAt: new Date(),
+  });
 
-const insertTask = taskText => TasksCollection.insert({ text: taskText });
+const SEED_USERNAME = "meteorite";
+const SEED_PASSWORD = "password";
 
 Meteor.startup(() => {
   if (!Accounts.findUserByUsername(SEED_USERNAME)) {
@@ -15,15 +20,17 @@ Meteor.startup(() => {
     });
   }
 
+  const user = Accounts.findUserByUsername(SEED_USERNAME);
+
   if (TasksCollection.find().count() === 0) {
     [
-      'First Task',
-      'Second Task',
-      'Third Task',
-      'Fourth Task',
-      'Fifth Task',
-      'Sixth Task',
-      'Seventh Task'
-    ].forEach(insertTask)
+      "First Task",
+      "Second Task",
+      "Third Task",
+      "Fourth Task",
+      "Fifth Task",
+      "Sixth Task",
+      "Seventh Task",
+    ].forEach((taskText) => insertTask(taskText, user));
   }
 });
